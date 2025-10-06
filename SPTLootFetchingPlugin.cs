1269ef689443216cb38751414f280a54682c7f7d
高耳鸡 <703821945@qq.com>
2025-10-07 04:15:25 +0800

    use TMP instead of IMGUI

--- SPTLootFetchingPlugin.cs
+++ SPTLootFetchingPlugin.cs
@@ -1,16 +1,22 @@
 ﻿using BepInEx;
 using BepInEx.Configuration;
-using EFT;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace SPTLootFetching {
-    [BepInPlugin("net.skydust.SPTLootFetchingPlugin", "SPTLootFetchingPlugin", "1.0.4")]
+    [BepInPlugin("net.skydust.SPTLootFetchingPlugin", "SPTLootFetchingPlugin", "1.0.5")]
     [BepInProcess("EscapeFromTarkov")]
     public class SPTLootFetchingPlugin : BaseUnityPlugin {
-        public static ConfigEntry<KeyboardShortcut>? Shortcut { get; private set; } = null;
+        public static Single DefaultDistance { get; } = 256F;
+
+        public static Color DefaultEspColor { get; } = new Color(94, 230, 144);
+
+        public static ConfigEntry<KeyboardShortcut>? Shortcut1 { get; private set; } = null;
+
+        public static ConfigEntry<KeyboardShortcut>? Shortcut2 { get; private set; } = null;
 
         public static ConfigEntry<Single>? Distance { get; private set; } = null;
 
@@ -18,23 +24,68 @@ namespace SPTLootFetching {
 
         public static ConfigEntry<Color>? ESPColor { get; private set; } = null;
 
-        public static ConfigEntry<KeyboardShortcut>? Shortcut2 { get; private set; } = null;
-
         public static ConfigEntry<KeyboardShortcut>? Shortcut3 { get; private set; } = null;
 
-        public static Single DefaultDistance{get;} = 256F;
+        public static ConfigEntry<KeyboardShortcut>? Shortcut4 { get; private set; } = null;
+
+        public static ConfigEntry<KeyboardShortcut>? Shortcut5 { get; private set; } = null;
 
         private AssemblyPatches_EFT__Interactive__LootItem.InitPatch? InitPatch { get; set; } = null;
 
         private Boolean IsBusy { get; set; } = false;
 
         protected void Awake () {
-            SPTLootFetchingPlugin.Shortcut = this.Config.Bind<KeyboardShortcut>("config", "shortcut", new KeyboardShortcut(KeyCode.Comma, KeyCode.LeftControl), "press to fetching loose loots in distance range");
-            SPTLootFetchingPlugin.Shortcut2 = this.Config.Bind<KeyboardShortcut>("config", "shortcut-2", new KeyboardShortcut(KeyCode.Period, KeyCode.LeftControl), "press to fetching loose loots in whole map");
-            SPTLootFetchingPlugin.Shortcut3 = this.Config.Bind<KeyboardShortcut>("config", "shortcut-3", new KeyboardShortcut(KeyCode.Slash, KeyCode.LeftControl), "press to fetching bots in whole map");
-            SPTLootFetchingPlugin.Distance = this.Config.Bind<Single>("config", "distance", SPTLootFetchingPlugin.DefaultDistance, new ConfigDescription("will culling if distance above this value", new AcceptableValueRange<Single>(1F, 1024F)));
-            SPTLootFetchingPlugin.ESPEnable = this.Config.Bind<Boolean>("ESP", "enable", false, String.Empty);
-            SPTLootFetchingPlugin.ESPColor = this.Config.Bind<Color>("ESP", "color", new Color(94, 230, 144), "loot name color, apply in next raid");
+            SPTLootFetchingPlugin.Shortcut1 = this.Config.Bind<KeyboardShortcut>(
+                "general",
+                "shortcut-1",
+                new KeyboardShortcut(KeyCode.Comma, KeyCode.LeftControl),
+                "press to fetching loose loots in distance range"
+            );
+            SPTLootFetchingPlugin.Shortcut2 = this.Config.Bind<KeyboardShortcut>(
+                "general",
+                "shortcut-2",
+                new KeyboardShortcut(KeyCode.Period, KeyCode.LeftControl),
+                "press to fetching loose loots in whole map"
+            );
+            SPTLootFetchingPlugin.Distance = this.Config.Bind<Single>(
+                "general",
+                "distance",
+                SPTLootFetchingPlugin.DefaultDistance,
+                new ConfigDescription(
+                    "will culling if distance above this value",
+                    new AcceptableValueRange<Single>(1F, 1024F)
+                )
+            );
+            SPTLootFetchingPlugin.ESPEnable = this.Config.Bind<Boolean>(
+                "general",
+                "esp-enable",
+                false,
+                String.Empty
+            );
+            SPTLootFetchingPlugin.ESPColor = this.Config.Bind<Color>(
+                "general",
+                "esp-color",
+                SPTLootFetchingPlugin.DefaultEspColor,
+                "loot name color, apply in next raid"
+            );
+            SPTLootFetchingPlugin.Shortcut3 = this.Config.Bind<KeyboardShortcut>(
+                "tools",
+                "shortcut-3",
+                new KeyboardShortcut(KeyCode.Keypad3, KeyCode.LeftControl),
+                "teleport a nearest bot around to you"
+            );
+            SPTLootFetchingPlugin.Shortcut4 = this.Config.Bind<KeyboardShortcut>(
+                "tools",
+                "shortcut-4",
+                new KeyboardShortcut(KeyCode.Keypad4, KeyCode.LeftControl),
+                "teleport you around to nearest bot"
+            );
+            SPTLootFetchingPlugin.Shortcut5 = this.Config.Bind<KeyboardShortcut>(
+                "tools",
+                "shortcut-5",
+                new KeyboardShortcut(KeyCode.Keypad5, KeyCode.LeftControl),
+                "teleport you to lighthouse area, only work in lighthouse map"
+            );
             this.InitPatch = new AssemblyPatches_EFT__Interactive__LootItem.InitPatch();
             this.Logger.LogDebug("plugin loaded");
         }
@@ -46,7 +97,7 @@ namespace SPTLootFetching {
 
         protected void Update () {
             if (this.IsBusy) { return; }
-            if (SPTLootFetchingPlugin.Shortcut?.Value.IsUp() == true) {
+            if (SPTLootFetchingPlugin.Shortcut1?.Value.IsUp() == true) {
                 this.FetchInRange(SPTLootFetchingPlugin.Distance?.Value ?? SPTLootFetchingPlugin.DefaultDistance);
                 return;
             }
@@ -55,7 +106,15 @@ namespace SPTLootFetching {
                 return;
             }
             if (SPTLootFetchingPlugin.Shortcut3?.Value.IsUp() == true) {
-                this.FetchBots();
+                this.TeleportBotToPlayer();
+                return;
+            }
+            if (SPTLootFetchingPlugin.Shortcut4?.Value.IsUp() == true) {
+                this.TeleportPlayerToBot();
+                return;
+            }
+            if (SPTLootFetchingPlugin.Shortcut5?.Value.IsUp() == true) {
+                this.TeleportPlayerToLighthouse();
                 return;
             }
         }
@@ -72,7 +131,7 @@ namespace SPTLootFetching {
             if (gameWorld.MainPlayer == null) { return; }
             this.IsBusy = true;
             Vector3 position = gameWorld.MainPlayer.Transform.TransformPoint(new Vector3(0F, 1F, 2F));
-            List<EFT.Interactive.LootItem> loots = gameWorld.LootItems.list_0.FindAll(
+            IEnumerable<EFT.Interactive.LootItem> loots = gameWorld.LootItems.list_0.Where(
                 x => x.isActiveAndEnabled
                 // cause OutOfRangeException
                 // && x.IsVisibilityEnabled
@@ -81,13 +140,13 @@ namespace SPTLootFetching {
                 && !x.Item.IsEmptyStack
             );
             if (distance.HasValue) {
-                // loots = loots.FindAll(x => Vector3.Distance(position, x.transform.position) <= distance);
-                loots = loots.FindAll(x => (x.transform.position - position).sqrMagnitude <= distance * distance);
+                loots = loots.Where(x => (x.transform.position - position).sqrMagnitude <= distance * distance);
             }
-            _ = this.StartCoroutine(this.TeleportLoots(loots, position));
+            // ToList() create a copy to avoid "collection modified" error
+            _ = this.StartCoroutine(this.TeleportLoots(loots.ToList(), position));
         }
 
-        private IEnumerator TeleportLoots (List<EFT.Interactive.LootItem> loots, Vector3 position) {
+        private IEnumerator TeleportLoots (IList<EFT.Interactive.LootItem> loots, Vector3 position) {
             foreach (EFT.Interactive.LootItem lootItem in loots) {
                 lootItem.Shift = Vector3.zero;
                 lootItem.transform.position = position + (UnityEngine.Random.onUnitSphere * 0.5F);
@@ -103,39 +162,83 @@ namespace SPTLootFetching {
             yield break;
         }
 
-        private void FetchBots () {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0046:转换为条件表达式", Justification = "<挂起>")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0053:使用表达式主体来表示 Lambda 表达式", Justification = "<挂起>")]
+        private void TeleportBotToPlayer () {
+            if (this.IsBusy) { return; }
             if (Comfort.Common.Singleton<EFT.GameWorld>.Instance == null) { return; }
             EFT.GameWorld gameWorld = Comfort.Common.Singleton<EFT.GameWorld>.Instance;
+            if (gameWorld.AllAlivePlayersList.Count < 1) { return; }
             if (gameWorld.MainPlayer == null) { return; }
             this.IsBusy = true;
-            Vector3 position = gameWorld.MainPlayer.Transform.TransformPoint(new Vector3(0F, 1F, 2F));
-            EFT.Player? singleBot = gameWorld.AllAlivePlayersList.Find(
-                x => x.ProfileId != gameWorld.MainPlayer.ProfileId
-                && !x.IsYourPlayer
-                && x.IsAI
-                && x.gameObject.activeInHierarchy
-                //&& x.GetComponent<EFT.EftGamePlayerOwner>()
-                && x.GetComponent<EFT.Interactive.Corpse>() == null
-                && x.GetComponent<EFT.LocalPlayer>() != null
-                && x.GetComponent<EFT.BotOwner>() != null
-            );
-            if(singleBot==null){
+            Vector3 position = gameWorld.MainPlayer.Transform.TransformPoint(new Vector3(0F, 2F, 2F));
+            EFT.Player? singleBot = gameWorld.AllAlivePlayersList.Where(item => {
+                if (item.IsYourPlayer || item.ProfileId == gameWorld.MainPlayer.ProfileId) { return false; }
+                if (!item.IsAI) { return false; }
+                if (!item.gameObject.activeInHierarchy) { return false; }
+                if (item.GetComponent<EFT.Interactive.Corpse>() != null) { return false; }
+                if (item.GetComponent<EFT.LocalPlayer>() == null) { return false; }
+                if (item.GetComponent<EFT.BotOwner>() == null) { return false; }
+                if (item.GetComponent<AIFirearmController>()?.Item?.TemplateId == "657857faeff4c850222dff1b") { return false; }
+                return true;
+            }).OrderBy(item => {
+                return (item.gameObject.transform.position - position).sqrMagnitude;
+            }).FirstOrDefault();
+            if (singleBot == null) {
                 this.IsBusy = false;
                 return;
             }
-            _ = this.StartCoroutine(this.TeleportBots(new List<EFT.Player> { singleBot }, position));
+            singleBot.GetComponent<EFT.BotOwner>()?.StopMove();
+            //botOwner.Mover.GoToPointNoWay(position);
+            singleBot.Teleport(position);
+            NotificationManagerClass.DisplayMessageNotification(String.Concat("bot <", singleBot.name, "> is around you"));
+            this.IsBusy = false;
         }
 
-        private IEnumerator TeleportBots (List<EFT.Player> bots, Vector3 position) {
-            foreach (EFT.Player bot in bots) {
-                bot.Teleport(position);
-                bot.GetComponent<EFT.BotOwner>().Settings.botDifficulty_0 = BotDifficulty.impossible;
-                NotificationManagerClass.DisplayMessageNotification(String.Concat("bot <",bot.name, "> fetched"));
-                yield return new WaitForSeconds(0.1F);// save CPU single core
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0046:转换为条件表达式", Justification = "<挂起>")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0053:使用表达式主体来表示 Lambda 表达式", Justification = "<挂起>")]
+        private void TeleportPlayerToBot () {
+            if (this.IsBusy) { return; }
+            if (Comfort.Common.Singleton<EFT.GameWorld>.Instance == null) { return; }
+            EFT.GameWorld gameWorld = Comfort.Common.Singleton<EFT.GameWorld>.Instance;
+            if (gameWorld.AllAlivePlayersList.Count < 1) { return; }
+            if (gameWorld.MainPlayer == null) { return; }
+            this.IsBusy = true;
+            Vector3 position = gameWorld.MainPlayer.Transform.position;
+            EFT.Player? singleBot = gameWorld.AllAlivePlayersList.Where(item => {
+                if (item.IsYourPlayer || item.ProfileId == gameWorld.MainPlayer.ProfileId) { return false; }
+                if (!item.IsAI) { return false; }
+                //if (!item.gameObject.activeInHierarchy) { return false; }
+                //if (item.GetComponent<EFT.Interactive.Corpse>() != null) { return false; }
+                if (item.GetComponent<EFT.LocalPlayer>() == null) { return false; }
+                if (item.GetComponent<EFT.BotOwner>() == null) { return false; }
+                if (item.GetComponent<AIFirearmController>()?.Item?.TemplateId == "657857faeff4c850222dff1b") { return false; }
+                return true;
+            }).OrderBy(item => {
+                return (item.gameObject.transform.position - position).sqrMagnitude;
+            }).FirstOrDefault();
+            if (singleBot == null) {
+                this.IsBusy = false;
+                return;
             }
-            //NotificationManagerClass.DisplayMessageNotification(String.Concat(bots.Count, " bots fetched"));
+            gameWorld.MainPlayer.Teleport(singleBot.Transform.position);
+            NotificationManagerClass.DisplayMessageNotification(String.Concat("you are around bot <", singleBot.name, ">"));
             this.IsBusy = false;
-            yield break;
+        }
+
+        private void TeleportPlayerToLighthouse () {
+            if (this.IsBusy) { return; }
+            if (Comfort.Common.Singleton<EFT.GameWorld>.Instance == null) { return; }
+            EFT.GameWorld gameWorld = Comfort.Common.Singleton<EFT.GameWorld>.Instance;
+            if (gameWorld.MainPlayer == null) { return; }
+            String sceneName = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
+            if (sceneName != "Lighthouse_Scripts") {
+                NotificationManagerClass.DisplayMessageNotification("current map is not LightHouse");
+                return;
+            }
+            this.IsBusy = true;
+            gameWorld.MainPlayer.Teleport(new Vector3(174.25F, 2.5F, 522.35F), true);
+            this.IsBusy = false;
         }
     }
 }
--- UnityComponents/LootNameLabelComponent.cs
+++ UnityComponents/LootNameLabelComponent.cs
@@ -1,69 +1,59 @@
 ﻿using System;
+using TMPro;
 using UnityEngine;
 
 namespace SPTLootFetching.UnityComponents {
     public class LootNameLabelComponent : MonoBehaviour {
-        public static GUIStyle GUIStyle { get; } = new GUIStyle() {
-            fontSize = 12,
-            alignment = TextAnchor.MiddleCenter,
-            normal = {
-                textColor = SPTLootFetchingPlugin.ESPColor?.Value ?? new Color(94, 136, 170),
-            }
-        };
-
-        public static GUIStyle GUIStyle2 { get; } = new GUIStyle() {
-            fontSize = 14,
-            alignment = TextAnchor.MiddleCenter,
-            normal = {
-                textColor = new Color(226, 207, 58),
-            }
-        };
-
         private EFT.Interactive.LootItem? LootItem { get; set; } = null;
 
-        private String? LootName { get; set; } = String.Empty;
+        private GameObject LabelObject{get;set;} = new GameObject();
 
-        private Vector3 ViewportPoint { get; set; } = Vector3.zero;
-
-        private Vector3 ScreenPoint { get; set; } = Vector3.zero;
-
-        private Vector2 TextPosition { get; set; } = Vector2.zero;
+        private TextMeshPro? LabelComponent{get;set;} = null;
 
         public void Awake () {
-            this.LootItem = this.GetComponentInParent<EFT.Interactive.LootItem>();
-            this.LootName = String.IsNullOrWhiteSpace(this.LootItem.Name.Localized()) ? this.LootItem.Name : this.LootItem.Name.Localized();
+            this.LootItem = this.gameObject.GetComponentInParent<EFT.Interactive.LootItem>();
+            String lootName = String.Concat("<", String.IsNullOrWhiteSpace(this.LootItem.Name.Localized()) ? this.LootItem.Name : this.LootItem.Name.Localized(), ">");
+            this.LabelObject.name = String.Concat("LootNameLabelObject",lootName);
+            this.LabelObject.transform.SetParent(this.gameObject.transform);
+            this.LabelComponent = this.LabelObject.GetOrAddComponent<TextMeshPro>();
+            Color color = SPTLootFetchingPlugin.ESPColor?.Value ?? new Color(94, 136, 170);
+            if (this.LootItem?.Item?.QuestItem ?? false) {
+                color = new Color(255, 165, 0);
+            }
+            this.LabelComponent.alignment = TextAlignmentOptions.Center;
+            this.LabelComponent.maxVisibleCharacters = 16;
+            this.LabelComponent.maxVisibleLines = 1;
+            this.LabelComponent.color = color;
+            this.LabelComponent.fontSize = 0.5F;
+            this.LabelComponent.enableAutoSizing = false;
+            this.LabelComponent.fontWeight = FontWeight.Thin;
+            this.LabelComponent.outlineWidth = 0.0625F;
+            this.LabelComponent.isOverlay = true;
+            //this.LabelComponent.isOrthographic = true;
+            this.LabelComponent.text = lootName;
+            this.LabelComponent.transform.localPosition = new Vector3(0,0.01F,0); // set anchor
+            this.LabelComponent.autoSizeTextContainer = true;
         }
 
-        public void Start () {
-            //
-        }
+        //public void Start () {}
 
-        public void Update () {
-            this.TextPosition = Vector2.zero;
-            if (SPTLootFetchingPlugin.ESPEnable?.Value != true) {return;}
-            if (this.LootItem == null || Camera.main == null) {return;}
-            Vector3 vector = this.gameObject.transform.position - Camera.main.gameObject.transform.position;
+        //public void Update () {}
+
+        public void LateUpdate () {
+            if(this.LabelComponent==null || Camera.main==null){return;}
             Single limit = SPTLootFetchingPlugin.Distance?.Value ?? SPTLootFetchingPlugin.DefaultDistance;
-            if (limit <= 0 || vector.sqrMagnitude > limit * limit) {return;}
-            // world to screen => https://discussions.unity.com/t/gui-label-follow-an-object/29578/2
-            this.ViewportPoint = Camera.main.WorldToViewportPoint(this.gameObject.transform.position);
-            if (this.ViewportPoint.x > 1F || this.ViewportPoint.x < 0F || this.ViewportPoint.y > 1F || this.ViewportPoint.y < 0F || this.ViewportPoint.z < 0F) {return;}
-            this.ScreenPoint = Camera.main.ViewportToScreenPoint(this.ViewportPoint);
-            this.TextPosition = new Vector2(this.ScreenPoint.x,Screen.height - this.ScreenPoint.y);
-        }
-
-        public void OnGUI () {
-            if (this.TextPosition == Vector2.zero) { return; }
-            GUI.Label(
-                new Rect(this.TextPosition.x - 64F, this.TextPosition.y - 12F, 128F, 24F),
-                String.Concat("<", this.LootName, ">"),
-                this.LootItem?.Item.QuestItem == true ? LootNameLabelComponent.GUIStyle2 : LootNameLabelComponent.GUIStyle
-            );
+            //Vector3 vector = this.gameObject.transform.position - Camera.main.gameObject.transform.position;
+            Single vector = Vector3.Distance(this.gameObject.transform.position,Camera.main.gameObject.transform.position);
+            //if (limit <= 0 || vector.sqrMagnitude > limit * limit) {return;}
+            if (limit <= 0 || vector > limit) {return;}
+            this.LabelComponent.transform.rotation = Camera.main.gameObject.transform.rotation;
+            this.LabelComponent.fontSize = 0.5F + (vector * 0.10625F);
         }
 
         public void OnDestroy () {
             this.LootItem = null;
-            this.LootName = null;
+            this.LabelComponent = null;
+            this.LabelObject.DestroyAllChildren();
         }
     }
 }
